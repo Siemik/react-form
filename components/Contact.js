0749ea9becd46b4a14052415b75920e30875c8b7
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2019-08-26 13:17:17 +0200

    Converted JS to JSA in Contact component

--- components/Contact.js
+++ components/Contact.js
@@ -4,17 +4,15 @@ var Contact = React.createClass({
   },
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: 'https://zenoncar.com/wp-content/uploads/2015/08/zenon-icons-profile-01.png'
-        }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto: '+ this.props.item.email},
-          this.props.item.email
-        )
-      )
+      <div className={'contactItem'}>
+          <img className={'contactImage'} src={'link-do-obrazka.png'}/>
+          <p className={'contactLabel'}>
+              Imię: {this.props.contact.firstName}
+          </p>
+          <a href={'mailto:' + this.props.item.email}>
+              {this.props.item.email}
+          </a>
+      </div>
     )
   },
 });
